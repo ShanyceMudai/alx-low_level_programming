6f2b6b4c521a2563556058a54d217d6c46f712d6
ShanyceMudai <jepchumbashanyce@gmail.com>
2023-08-10 15:17:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 * main-prints output using put
 *
 * Return: always(0)
+*
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-}	
+}
+
 
