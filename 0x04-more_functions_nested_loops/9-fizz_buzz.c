98f2eadb52195420eaa4b3fc26ee8421785fa949
ShanyceMudai <jepchumbashanyce@gmail.com>
2023-08-18 13:23:50 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int j;
 
-	for (j = 1; j <= 100; j++)
+	for (j = 1; j < 100; j++)
 	{
-		if ((j % 3 == 0) && (j % 5 == 0))
+		if ((j % 3) == 0 && (j % 5) == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -28,6 +28,7 @@ int main(void)
 			printf("%d ", j);
 		}
 	}
+	printf("Buzz");
 	printf("\n");
 
                 return (0);
