db79e5ea0b4c125e47493ecbac04e0cd29efbc84
ShanyceMudai <jepchumbashanyce@gmail.com>
2023-09-12 09:42:48 -0700

    struct containing information about a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct containing information about dogs
+ *
+ * @name: name of the dog
+ *
+ * @age: age of the dog
+ *
+ * @owner: owner of the dog
+ *
+ * Description: header file
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* dog.h */
